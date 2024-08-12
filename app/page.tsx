b3d2a9d6cabe3dca666d723a832802a6004a11c6
Shoaib Hossain <shoaibhossain24@gmail.com>
2024-08-12 23:07:24 +0600

    chore: Add initial project files and configurations

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import InputForm from '@/components/InputForm'
+import React from 'react'
+
+const page = () => {
+  return (
+    <section>
+      <div>
+        <InputForm />
+      </div>
+    </section>
+  )
+}
+
+export default page
